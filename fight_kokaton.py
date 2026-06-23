cbac0d9cd3a7aa0a71de94e06afcfc19f2470605
c0a251085c <c0a251085@edu.teu.ac.jp>
2026-06-23 14:37:52 +0900

    練習３:よろこびエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -166,9 +166,11 @@ def main():
                 time.sleep(1)
                 return
 
-        if bomb is not None:
+        if bomb is not None:  
             if beam is not None :
-                if beam.rct.colliderect(bomb.rct):
+                if beam.rct.colliderect(bomb.rct):   #ビームが爆弾に当たったら
+                   bird.change_img(6, screen)
+                   pg.display.update()
                    bomb = None  
                    beam = None 
 
